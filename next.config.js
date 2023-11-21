176b328ee977a61b706c96fc3d9444837d911a27
chethancheths <chethancheths13@gmail.com>
2023-11-21 21:16:36 +0530

    config revert

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
-module.exports = {
-    // Add the assetPrefix in your next.config.js
-    // This is required to set up the correct base path
-    basePath: '/my-portfolio', // Replace with your repository name
-    assetPrefix: '/my-portfolio/', // Replace with your repository name
-  }
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports = nextConfig
